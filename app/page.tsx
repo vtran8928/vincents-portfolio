d20334d0957b88e354a4914586bab7ea95e96b83
vtran8928 <vincenttran8928@gmail.com>
2026-01-23 14:17:47 -0500

    short message describing the change

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
 
             <div className="flex flex-wrap gap-4">
               <a
-                href="https://drive.google.com/file/d/16XDHu6wh4eChELvsr5DqF4_GjJpgLVKP/view?usp=sharing"
+                href="https://drive.google.com/file/d/1-61T_1w71fF-S-OkJt6UX4D4MqKhrqpN/view?usp=sharing"
                 className="px-7 py-3 rounded-lg bg-emerald-600 text-neutral-950 font-medium hover:bg-emerald-500 transition-colors duration-300"
               >
                 Resume
